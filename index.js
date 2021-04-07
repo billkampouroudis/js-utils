ac28fe255479526a5f4295acfb56cc6ddedb9dc0
Bill Kampouroudis <v.kampouroudis@gmail.com>
2021-04-08 01:07:51 +0300

    Remove convertions to number

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ import number from './src/is/number.js';
 import object from './src/is/object.js';
 import truthy from './src/is/truthy.js';
 
-import toInteger from './src/convert/toInteger.js';
-import toNumber from './src/convert/toNumber.js';
-
 import safe from './src/get/safe.js';
 
 import debounce from './src/misc/debounce.js';
@@ -21,11 +18,6 @@ export const is = {
   number,
   object,
   truthy,
-}
-
-export const convert = {
-  toInteger,
-  toNumber
 }
 
 export const get = {
