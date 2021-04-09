24df3260e2e42c2e96e8fcb6f5fc98f9ed42596d
Bill Kampouroudis <v.kampouroudis@gmail.com>
2021-04-09 23:33:54 +0300

    Add more tests for get.safe

--- __test__/get/safe.test.js
+++ __test__/get/safe.test.js
@@ -3,19 +3,37 @@ import safe from '../../src/get/safe.js';
 describe('Get a child attribute safely', () => {
   const foo = {
     id: 1,
-    bar: undefined
+    bar: undefined,
+    obj: {},
+    empty: null,
+    string: 'abcdefg'
   };
 
-  it('Should handle the error returning undefined', () => {
+  it('Should return undefined when trying to access a child of an undefined object.', () => {
     expect(safe(() => foo.bar.child)).toBe(undefined);
   });
 
-  it('Should handle the error returning a fallback value', () => {
+  it('Should return the given fallback when trying to access a child of an undefined object.', () => {
     expect(safe(() => foo.bar.child, 'fallback')).toBe('fallback');
   });
 
-  it('Should return the actual child value', () => {
-    expect(safe(() => foo.id, 'fallback')).toBe(1);
+  it('Should return the actual child value when object is not undefined.', () => {
+    expect(safe(() => foo.id, 'fallback')).toBe(foo.id);
   });
 
+  it('Should return empty object when child is empty object', () => {
+    expect(safe(() => foo.obj)).toEqual(foo.obj);
+  });
+
+  it('Should return null when child is null', () => {
+    expect(safe(() => foo.empty)).toBe(foo.empty);
+  });
+
+  it('Should return number when child a number', () => {
+    expect(safe(() => foo.id)).toBe(foo.id);
+  });
+
+  it('Should return string when child a string', () => {
+    expect(safe(() => foo.string)).toBe(foo.string);
+  });
 });
