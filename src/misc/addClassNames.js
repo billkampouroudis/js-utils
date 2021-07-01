40c709800a789715044a562093a5c6751be87715
Bill Kampouroudis <v.kampouroudis@gmail.com>
2021-07-01 18:58:50 +0300

    Make classnames return undefined when nothing is provided

--- src/misc/addClassNames.js
+++ src/misc/addClassNames.js
@@ -1,6 +1,10 @@
 /**
   * Adds the given CSS classes to an element.
   *
+  * NOTE: This function should be used only with React.js
+  *       because in case that no classes are rendered, it will return undefined.
+  *       In React.js this means that the tag "className" will not be added in the element at all
+  *
   * @param {string} standardClasses The CSS classes that will for sure be added
   * @param {object} conditionalClasses The CSS classes that will be rendered based on a condition
   * @example
@@ -19,5 +23,5 @@ export default function addClassNames(standardClasses = '', conditionalClasses =
     }
   }
 
-  return finalClasses;
+  return finalClasses || undefined;
 }
